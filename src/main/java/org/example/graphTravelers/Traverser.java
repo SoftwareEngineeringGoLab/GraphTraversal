00f45d63a92558d2f824bde0b1edb1dc7ce4fd3f
erfan <sadraiyeh.e2002@gmail.com>
2025-06-14 13:15:21 +0330

    Update Traverser

--- src/main/java/org/example/graphTravelers/Traverser.java
+++ src/main/java/org/example/graphTravelers/Traverser.java
@@ -1,7 +1,9 @@
 package org.example.graphTravelers;
 
 import java.util.List;
+import org.example.adapter.GraphAdapter;
 
 public interface Traverser {
     List<Integer> traverse(Integer startVertex);
+    void setGraphAdapter(GraphAdapter<Integer, String> adapter);
 }
